668380463be3884528d0ebebff0c4cde04c35fd0
matthewmilitante <matthewmilitante@gmail.com>
2014-01-18 12:58:33 -0500

    added some frames

--- gen/com/example/pinly/R.java
+++ gen/com/example/pinly/R.java
@@ -27,16 +27,18 @@ public final class R {
     public static final class drawable {
         public static final int highlight=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int logotran=0x7f020002;
-        public static final int main_button=0x7f020003;
-        public static final int main_button1=0x7f020004;
-        public static final int main_title=0x7f020005;
-        public static final int snap=0x7f020006;
+        public static final int logo2=0x7f020002;
+        public static final int logotran=0x7f020003;
+        public static final int main_button=0x7f020004;
+        public static final int main_button1=0x7f020005;
+        public static final int main_title=0x7f020006;
+        public static final int snap=0x7f020007;
     }
     public static final class id {
         public static final int about=0x7f090007;
-        public static final int action_settings=0x7f090013;
+        public static final int action_settings=0x7f090016;
         public static final int add=0x7f090005;
+        public static final int bottom=0x7f090013;
         public static final int button_main_lay=0x7f090003;
         public static final int checkBox1=0x7f09000a;
         public static final int checkBox2=0x7f09000b;
@@ -47,19 +49,22 @@ public final class R {
         public static final int imageView1=0x7f090002;
         public static final int main_lin=0x7f090001;
         public static final int main_scroll=0x7f090000;
+        public static final int middle=0x7f090012;
         public static final int opB1=0x7f090010;
         public static final int outfit=0x7f090004;
-        public static final int random=0x7f090011;
+        public static final int random=0x7f090014;
         public static final int remove=0x7f090006;
-        public static final int removeView=0x7f090012;
+        public static final int removeView=0x7f090015;
         public static final int textView1=0x7f090008;
         public static final int textView2=0x7f090009;
+        public static final int top=0x7f090011;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int options_activity=0x7f030001;
-        public static final int outfits_activity=0x7f030002;
-        public static final int remove_activity=0x7f030003;
+        public static final int about_activity=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int options_activity=0x7f030002;
+        public static final int outfits_activity=0x7f030003;
+        public static final int remove_activity=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f080000;
