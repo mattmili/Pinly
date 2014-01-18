c6cfb32c583c68b98666a448b9624b5a79203b93
CollinStubbs <collin.stubbs@uoit.net>
2014-01-18 02:52:50 -0500

    Added title to main activity

--- gen/com/example/pinly/R.java
+++ gen/com/example/pinly/R.java
@@ -27,18 +27,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int main_button=0x7f020001;
         public static final int main_button1=0x7f020002;
-        public static final int title_border=0x7f020003;
+        public static final int main_title=0x7f020003;
     }
     public static final class id {
         public static final int about=0x7f090007;
         public static final int action_settings=0x7f090008;
         public static final int add=0x7f090005;
         public static final int button_main_lay=0x7f090003;
+        public static final int imageView1=0x7f090002;
         public static final int main_lin=0x7f090001;
         public static final int main_scroll=0x7f090000;
         public static final int outfit=0x7f090004;
         public static final int remove=0x7f090006;
-        public static final int textView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
