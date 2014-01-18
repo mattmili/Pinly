23d38b91fc1058b05418e07f2bf24aa8ed3c7d7c
CollinStubbs <collin.stubbs@uoit.net>
2014-01-18 02:06:27 -0500

    rough main activity

--- gen/com/example/pinly/R.java
+++ gen/com/example/pinly/R.java
@@ -10,6 +10,9 @@ package com.example.pinly;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int main_back=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +20,35 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int main_button=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int about=0x7f090005;
+        public static final int action_settings=0x7f090006;
+        public static final int edit=0x7f090004;
+        public static final int main_lin=0x7f090001;
+        public static final int main_scroll=0x7f090000;
+        public static final int outfit=0x7f090003;
+        public static final int textView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int about=0x7f060005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int edit=0x7f060004;
+        public static final int hello_world=0x7f060002;
+        public static final int outfit=0x7f060003;
     }
     public static final class style {
         /** 
@@ -59,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
