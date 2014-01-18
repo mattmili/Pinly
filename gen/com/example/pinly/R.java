a85db74c17cfefbef18c66016a4dd3881239d05a
CollinStubbs <collin.stubbs@uoit.net>
2014-01-18 02:35:17 -0500

    Changed main activity

--- gen/com/example/pinly/R.java
+++ gen/com/example/pinly/R.java
@@ -26,14 +26,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int main_button=0x7f020001;
+        public static final int main_button1=0x7f020002;
+        public static final int title_border=0x7f020003;
     }
     public static final class id {
-        public static final int about=0x7f090005;
-        public static final int action_settings=0x7f090006;
-        public static final int edit=0x7f090004;
+        public static final int about=0x7f090007;
+        public static final int action_settings=0x7f090008;
+        public static final int add=0x7f090005;
+        public static final int button_main_lay=0x7f090003;
         public static final int main_lin=0x7f090001;
         public static final int main_scroll=0x7f090000;
-        public static final int outfit=0x7f090003;
+        public static final int outfit=0x7f090004;
+        public static final int remove=0x7f090006;
         public static final int textView1=0x7f090002;
     }
     public static final class layout {
@@ -45,10 +49,11 @@ public final class R {
     public static final class string {
         public static final int about=0x7f060005;
         public static final int action_settings=0x7f060001;
+        public static final int add=0x7f060004;
         public static final int app_name=0x7f060000;
-        public static final int edit=0x7f060004;
         public static final int hello_world=0x7f060002;
         public static final int outfit=0x7f060003;
+        public static final int remove=0x7f060006;
     }
     public static final class style {
         /** 
