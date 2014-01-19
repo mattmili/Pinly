e91a43b86d767c9ad0aa59e5562c8c014841385f
CollinStubbs <collin.stubbs@uoit.net>
2014-01-18 22:31:11 -0500

    added brief faq and about

--- src/com/example/pinly/about.java
+++ src/com/example/pinly/about.java
@@ -18,8 +18,12 @@ public class about extends Activity {
         TextView about_us = (TextView) findViewById(R.id.au);
         TextView faq = (TextView) findViewById(R.id.faq);
         
-        about_us.setText("\t\t FOR NEXT TIME");
-        faq.setText("IT'S AN APP");
+        about_us.setText("\t\t Pinly was developed by three students (Matthew Weeks, Matthew Militante, and Collin Stubbs) from the University of Ontario Institute of Technology."+
+        " It was designed to be part of the MHACKS III competition and incorporates our knowledge of databases and android development.");
+        faq.setText("\t\tWelcome to Pinly! With this application your long and frustrating hours of picking out the right outfit for the day will be gone!"+
+        " Pinly lets you take pictures of your clothes, store them on your phone, and generate random outfits based on what the weather is like."+
+        		" This app requires access to your camera and image files, this is only for storing and generation of the outfits, we do not do anything further with your files."+
+        		" If you wish to learn more about the project, or if you are an aspiring mobile developer looking to know how Pinly was built, our github at https://github.com/matthewmilitante/pinly.");
     }
 
 
