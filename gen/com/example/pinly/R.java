e0db550a2b4d0b36a200604b6e2c55f9b381d748
CollinStubbs <collin.stubbs@uoit.net>
2014-01-18 21:27:39 -0500

    add spinner

--- gen/com/example/pinly/R.java
+++ gen/com/example/pinly/R.java
@@ -36,14 +36,15 @@ public final class R {
         public static final int main_button1=0x7f020007;
         public static final int main_title=0x7f020008;
         public static final int snap=0x7f020009;
-        public static final int test=0x7f02000a;
+        public static final int spin_object=0x7f02000a;
+        public static final int test=0x7f02000b;
     }
     public static final class id {
         public static final int about=0x7f09000b;
-        public static final int action_settings=0x7f090019;
+        public static final int action_settings=0x7f09001a;
         public static final int add=0x7f090009;
         public static final int au=0x7f090003;
-        public static final int bottom=0x7f090016;
+        public static final int bottom=0x7f090017;
         public static final int button_main_lay=0x7f090007;
         public static final int checkBox1=0x7f09000c;
         public static final int checkBox2=0x7f09000d;
@@ -53,18 +54,19 @@ public final class R {
         public static final int checkBox6=0x7f090011;
         public static final int faq=0x7f090001;
         public static final int imageView1=0x7f090006;
-        public static final int imageView2=0x7f090013;
+        public static final int imageView2=0x7f090014;
         public static final int main_lin=0x7f090005;
         public static final int main_scroll=0x7f090004;
-        public static final int middle=0x7f090015;
-        public static final int opB1=0x7f090012;
+        public static final int middle=0x7f090016;
+        public static final int opB1=0x7f090013;
         public static final int outfit=0x7f090008;
-        public static final int random=0x7f090017;
+        public static final int random=0x7f090018;
         public static final int remove=0x7f09000a;
-        public static final int removeView=0x7f090018;
+        public static final int removeView=0x7f090019;
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090002;
-        public static final int top=0x7f090014;
+        public static final int top=0x7f090015;
+        public static final int type_spin=0x7f090012;
     }
     public static final class layout {
         public static final int about_activity=0x7f030000;
