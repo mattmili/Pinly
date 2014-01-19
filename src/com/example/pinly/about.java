a202a8e5cc4aca9b36dd1a468ba7bf51b5331e78
matthewmilitante <matthewmilitante@gmail.com>
2014-01-18 19:26:07 -0500

    test commit for about.java

---
+++ src/com/example/pinly/about.java
@@ -0,0 +1,28 @@
+package com.example.pinly;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class about extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+        TextView about_us = (TextView) findViewById(R.id.au);
+        TextView faq = (TextView) findViewById(R.id.faq);
+        
+        about_us.setText("MATT, MATT, COLLIN");
+        faq.setText("IT'S AN APP");
+    }
+
+
+
+    
+}
